b8e9d576a1d531be6b7747fb93fd066c2bb63bcc
codinnovation <admin@codinnovations.com>
2026-03-01 20:03:32 +0000

    feat: initialize root layout with global styles, metadata, and context providers.

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import { HighlyRatedEquipmentProvider } from "@/context/highlyRatedContext";
 import { RecommendationsEquipmentProvider } from "@/context/recommendationsContext";
 
 export const metadata: Metadata = {
-  title: "Warp5 Construction Equipment Rental",
+  title: "Warp5 Construction Equipments Rental",
   description: "Rent construction equipment with ease. Browse our extensive catalog, compare prices, and find the perfect machinery for your project. Fast, reliable, and affordable rentals at your fingertips.",
 };
 
